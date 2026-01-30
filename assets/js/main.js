47b5c1c813f30e42098ca64d4259d6b7a16f93e8
Eduardo Veiga <eduardozabeu@hotmail.com>
2026-01-30 19:02:14 -0300

    30/01/2026 - 19:02

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,7 +41,7 @@ const whatsappBtn = document.getElementById('whatsappBtn');
 if (whatsappBtn) {
     whatsappBtn.addEventListener('click', function(e) {
         e.preventDefault();
-        window.open('https://wa.me/5527998395903?text=Olá!%20Gostaria%20de%20falar%20com%20um%20especialista%20da%20Vertice%20Assessoria%20Imobiliária.', '_blank');
+        window.open('https://wa.me/5527998395903?text=Olá!%20Gostaria%20de%20falar%20com%20um%20especialista%20da%20Tekoah%20Assessoria%20Imobiliária.', '_blank');
     });
 }
 // Validação simples do formulário de contato
