e39a0964185601736b4c1564313813278a3e769d
Eduardo Veiga <eduardozabeu@hotmail.com>
2026-01-30 07:15:29 -0300

    30/01/2026 -  7:15

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ const whatsappBtn = document.getElementById('whatsappBtn');
 if (whatsappBtn) {
     whatsappBtn.addEventListener('click', function(e) {
         e.preventDefault();
-        window.open('https://wa.me/5573999999999?text=Olá!%20Gostaria%20de%20falar%20com%20um%20especialista%20do%20SAC%20Imobiliário.', '_blank');
+        window.open('https://wa.me/5527998395903?text=Olá!%20Gostaria%20de%20falar%20com%20um%20especialista%20da%20Vertice%20Assessoria%20Imobiliária.', '_blank');
     });
 }
 // Validação simples do formulário de contato
